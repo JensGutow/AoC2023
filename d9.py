5c608c14b8c13877f1c010d76ab5a56cec00278e
Jens Gutow <jens_gutow@web.de>
2023-12-09 14:23:44 +0100

    opt

--- d9.py
+++ d9.py
@@ -1,16 +1,20 @@
+import time
+
 def read_puzzle(file):
     return [list(map(int, line.split())) for line in open(file).read().splitlines() ]
 
 def find_nr_in_list(values):
-    ends = []
+    value = 0
     while any(i!=0 for i in values):
-        ends.append(values[-1])
-        values = [values[i+1] - values[i] for i in range(len(values)-1)]
-    return sum(ends)
+        value += values[-1]
+        values = [v2 - v1 for v1, v2 in zip(values, values[1:])]
+    return value
 
 def solve1(puzzle):
     n1 = sum([find_nr_in_list(l) for l in puzzle])
     n2 = sum([find_nr_in_list(l[::-1]) for l in puzzle])
     return n1, n2
 
+time_start = time.perf_counter()
 print("Task 1/2", solve1(read_puzzle('d9.txt')))
+print(f'Solved in {time.perf_counter()-time_start:.5f} Sec.')
