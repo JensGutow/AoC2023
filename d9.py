2bde4b9220d8215698242887cc15bb3fb015717b
Jens Gutow <jens_gutow@web.de>
2023-12-09 13:53:50 +0100

    Optimierung

--- d9.py
+++ d9.py
@@ -3,17 +3,12 @@ def read_puzzle(file):
 
 def find_nr_in_list(values):
     n = 0
-    start_values = []
+    ends = []
     while any(i!=0 for i in values):
         n += 1
-        start_values.append(values[0])
+        ends.append(values[-1])
         values = [values[i+1] - values[i] for i in range(len(values)-1)]
-    values.append(0)
-    for i in range(n):
-        l1 = [start_values[n-i-1]]
-        for v in values: l1.append(l1[-1]+v)
-        values = l1
-    return values[-1]
+    return sum(ends)
 
 def solve1(puzzle):
     n1 = sum([find_nr_in_list(l) for l in puzzle])
