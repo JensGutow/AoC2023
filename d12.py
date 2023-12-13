ae924da22a4840811e872a675b7c72ddaed209cc
Jens Gutow <jens_gutow@web.de>
2023-12-13 05:41:22 +0100

    d12, part2 recurvive + @functools.cache

--- d12.py
+++ d12.py
@@ -1,48 +1,39 @@
-import copy, re
+import copy, re, functools
 def read_puzzle(file):
     out = []
     for line in open(file).read().splitlines():
         cond_record, groups = line.split()
-        groups = list(map(int,groups.split(",")))
+        groups = tuple(map(int,groups.split(",")))
         out.append([cond_record, groups])
     return out
 
 def test_arrangement(test, groups):
     test2 = copy.copy(test)
-    test = re.findall("#[#?]*", test)
-    check2 = [len(s) for s in test ]
-    if "?" in test2:
-        result  = len(check2) <= len(groups)
-    else:
-        result  = len(check2) == len(groups)
+    test = re.findall("[#?]+", test)
+    check2 = [len(s) for s in test]
+    result = True if "?" in test2 else len(check2) == len(groups)
     if result:
         for sub, a,b in zip(test, check2, groups):
-            if "?" in sub:
-                sub_check = re.findall("#+", sub)
-                if sub_check:
-                    if len(sub_check[0]) > b:
-                        result = False
+            if not "?" in sub:
+                if a != b:
+                    result = False
+                    break
             else:
-                result = a == b
-            if not result: break
-    else:
-        pass
-    #print(test2, result, groups)
+                break
     return result
 
-def find_arrangement(line):
-    cond_record, groups = line
-    #print("org")
-    #print(cond_record, groups)
-    #print("---")
+def find_arrangement(cond_record, groups):
     results = set()
     results.add(cond_record)
     weiter = "?" in cond_record
-
+    inx_old = inx = 0
+    
     while weiter:
         weiter = False
         temp = set()
-        #print("-----------------")
+        if inx_old != inx:
+            print(inx)
+            inx_old = inx
         while results:
             cond_record = results.pop()
             if not "?" in cond_record: 
@@ -60,18 +51,62 @@ def find_arrangement(line):
                 if test_arrangement(test1, groups):temp.add(test1)
                 if test_arrangement(test2, groups):temp.add(test2)
         results = results.union(temp)
-        #print(inx)
-
     return results
 
+# importand for part2
+# saves known solution for lower arguments -> strong reduction of the necessary amount of calculation
+# the function parameter are used as a key for an internal dictionary.
+# if the function is called with arguments wich is saved in the dictonary, then the dict value 
+# is returned instead of the whole function is calculated.
+@functools.cache
+def amounts(conds, gr):
+    # eliminate all leading "." (have no influence)
+    conds = conds.lstrip(".")
+
+    # conds are empty: 
+    #  -> gr is empty: counting is successfully -> return 1 (current search str is a valid combination)
+    #  -> else: proof of failed -> current combination is invalid -> return 0
+    if not conds: return not gr
+
+    # groups are empty
+    # when the "rest" conds contains only "?" or "." -> return 1 (its a valid combination)
+    # but when the "rest" conds contains one ore more "#" -> conds/gr doesnt match -> return 0
+    if not gr: return not "#" in conds
+    
+    ## if the first chare is a "#"
+    if conds[0] == '#':
+        # invalid condtions are (means: the current combination is invalid -> return 0)
+        # - the rest conds has fewer chars as the first number in gr
+        #   len(conds) < gr[0]
+        # - there are no gr[0] consecutive # possible (the max amount of possibe # is to small)
+        #  ('.' in conds[:gr[0]])
+        #           <------- gr[0] ---------->
+        # conds:    #******.******************       *: # or ?
+        # - the minimal amount of possible # is to big (conds[gr[0]] == '#')
+        #           <------- gr[0] --------->
+        # conds:    #************************#       *: # or ?
+        if  len(conds) < gr[0] or '.' in conds[:gr[0]] or conds[gr[0]] == '#': 
+            return 0
+        # now: all checks are passed, the number gr[0] is proceeded, also the 
+        # next gr[0] chars from conds are proceeded
+        # examine the amounts for the rest of conds and gr
+        return amounts(conds[gr[0] + 1:], gr[1:])
+
+    # first char is a "?"
+    # -> ? is replaced with a "#" and a "." (result = sum of two counts)
+    # no numbers from gr are used up here
+    return amounts("#"+ conds[1:], gr) + amounts(conds[1:], gr)
 
 def solve1(puzzle):
-    n = 0
+    n1 = n2 = 0
     for line in puzzle:
-        menge = find_arrangement(line)
-        print(len(menge))
-        n += len(menge)
-    return n
+        cond_record, groups = line
+        # avoid an error if an index acces after the end of line -> add a neutral "." 
+        n1 += (amounts(cond_record+".", groups))
+        conds2 = ((cond_record+"?")*4)+cond_record
+        groups2 = groups * 5
+        n2 += (amounts(conds2+".", groups2))
+    return n1, n2
 
 puzzle = read_puzzle('d12.txt')
 
