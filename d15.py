4ed65dc6d5d972edc63a42c1a8f384fcfb215843
Jens Gutow <jens_gutow@web.de>
2023-12-15 17:15:32 +0100

    redesign (use Class)

--- d15.py
+++ d15.py
@@ -1,7 +1,32 @@
 import re 
+from collections import defaultdict
+
 def read_puzzle(file):
     return "".join(open(file).read().splitlines()).split(",")
 
+class Box:
+    def __init__(self, i):
+        self.box_nr = i
+        self.objectives = defaultdict(int)
+
+    def update(self, label, focus):
+        self.objectives[label] = focus
+
+    def delete(self, label):
+        self.objectives.pop(label, None)
+
+    def getFocus(self):
+        return (self.box_nr+1)*(sum([v*(j+1) for j, v in enumerate(self.objectives.values())]))
+
+    def __str__(self):
+        s=""
+        if self.objectives:
+            s += "Box " + str(self.box_nr) +":"
+            for label in self.objectives.keys():
+                s += "["+ label + str(self.objectives[label]) +"] "
+            s+="\n"
+        return s 
+    
 def calcHash(s):
     v = 0
     for c in s:
@@ -13,65 +38,25 @@ def calcHash(s):
 def solve1(puzzle):
     return sum([calcHash(s) for s in puzzle])
 
-def update_objectiv(box, label, objectiv):
-    labels, objectives = box
-    if label in labels: 
-        inx = labels.index(label)
-        objectives[inx] = (label, objectiv)
-    else:
-        labels.append(label)
-        objectives.append((label, objectiv))
-    return (labels, objectives)
-
-def remove_objectiv(box, label):
-    labels, objectives = box
-    if label in labels:
-        inx = labels.index(label)
-        labels.pop(inx)
-        objectives.pop(inx)
-    return (labels, objectives)
-
 def print_boxes(boxes, p):
     print("after ", p)
     for i, box in enumerate(boxes):
-        labels, objectives = box
-        if labels:
-            print("Box ", i,":", end="")
-            for objectiv in objectives:
-                label, nr = objectiv
-                print("[", label, nr,"] ", end="")
-            print("")
-    print("")
-
-def get_focus(boxes):
-    focus = 0
-    for b, box in enumerate(boxes):
-        labels, objectives = box
-        if labels:
-            for o, objectiv in enumerate(objectives):
-                _, focus_o = objectiv
-                focus_lokal = (b+1)*(o+1)*focus_o
-                focus += focus_lokal
-    return focus
+        print(box, end="")
 
 def solve2(puzzle):
-    boxes = [([], []) for i in range(256)]
+    boxes = [Box(i) for i in range(256)]
     for p in puzzle:
         label = re.findall("[a-zA-Z]+",p)[0]
         box_nr = calcHash(label)
         if  "=" in p:
             objectiv = int(re.findall("[\d]+",p)[0])
-            box = update_objectiv(boxes[box_nr], label, objectiv)
+            boxes[box_nr].update(label, objectiv)
         else:
-            box = remove_objectiv(boxes[box_nr], label)
-        boxes[box_nr] = box
+            boxes[box_nr].delete(label)
         #print_boxes(boxes, p)
-    return get_focus(boxes)
+    return sum([box.getFocus() for box in boxes])
         
-
-
 puzzle = read_puzzle('d15.txt')
-
 
 print("Task 1", solve1(puzzle))
 print("Task 2", solve2(puzzle))
