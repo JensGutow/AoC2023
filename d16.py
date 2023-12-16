031cd05b057ef402cf3b044bf8bab7a6127045fd
Jens Gutow <jens_gutow@web.de>
2023-12-16 12:27:08 +0100

    Optimierung, Kommmentare

--- d16.py
+++ d16.py
@@ -2,8 +2,15 @@ from collections import defaultdict
 
 Y_MAX = X_MAX = 0
 Y_MIN = X_MIN = 1e10 
+
+# DIRS: "DIR":(dx,dy) : welche Richtung ein Strahl einer Richtung in einem Schritt ausführt
 DIRS = {"N":(0,-1),"E":(1,0),"S":(0,1),"W":(-1,0)}
-DIRS_C = "NESW"     
+
+# keys: (DIR, C)
+#    DIR: Strahlrichtung, die in eine Zelle reingeht
+#      C: Codierung der Zelle (ein Zeichen aus "|-/\.")
+# values:
+#   Menge von möglichen Strahlrichtungen, wenn der Stahl die Zelle verlässt
 BEAM_RULES = {("N", "|"):"N" , ("S", "|"):"S", ("E", "|"):"SN", ("W", "|"):"SN",
               ("N", "-"):"EW", ("S", "-"):"EW",("E", "-"): "E", ("W", "-"):"W",
               ("N", "/"):"E" , ("S", "/"):"W", ("E", "/"): "N", ("W", "/"):"S",
@@ -48,18 +55,31 @@ def get_energy(puzzle):
     return sum([1 for _, dirs in puzzle.values() if dirs])
 
 def calc_energy(puzzle,start, dir):
+    # modifizierte Breitensuche
+    # puzzle: Dictonary (pos=x,y):(Zeichen, Richtungen von Strahlen, die die Zelle passiert haben)
+    # start: (äussere) Zelle, die vom Strahl als erstes getroffen wird
+    # dir: in welcher Richtung der Strahl verläuft
     visited = defaultdict(set) # pos:dirs
     queue = defaultdict(set) # pos:dirs
     queue[start] = dir
     while queue:
         pos = list(queue.keys())[0]
         dirs = queue.pop(pos)
+        # nächsten Eintrag wenn:
+        #   - es gibt einen "pos" Eintrag in visited UND
+        #   - alle dirs sind dort schon eingetragen
         if set(dirs).issubset(visited[pos]): continue
+        # Values vom puzzle dict sind: (das aktuelle Zeichen, bisherige Strahl-Richtungen)
         p_c, p_dirs = puzzle[pos]
+        # update der Strahl-Richtungen im Puzzle (max. hinzufügen - nicht löschen)
         puzzle[pos] = (p_c, p_dirs.union(dirs))
+        #optimierung: nur die die dirs behandeln, die nicht in visited[pos] enthalten sind
+        dirs =  "".join(list((set(dirs) - set(visited[pos]))))
         visited[pos] = "".join(set(visited[pos]).union(dirs))
         for next_pos_dir in  get_nb(puzzle, pos, dirs):
             n_pos, n_dir = next_pos_dir
+            # update der dirs für n_pos (wenn Eintrag existiert)
+            # oder einen neuen Eintrag einfügen
             queue[n_pos] = queue[n_pos].union(set(n_dir))
     #print_energy_status(puzzle)
     return get_energy(puzzle)
@@ -88,7 +108,7 @@ def solve2(puzzle):
             if ev > energy_value:
                 energy_value = ev
                 pos = start
-            #print(dir, start, ev)
+            print(dir, start, ev)
 
     return energy_value, pos
 
