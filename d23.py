93d9419f6c4f19fb27b3ebe4858074d08615eb3d
Jens Gutow <jens_gutow@web.de>
2023-12-23 19:31:41 +0100

    Rekursion, mit kleinen Optimierungen - aber  nicht ausreichend für Part 2

--- d23.py
+++ d23.py
@@ -1,4 +1,4 @@
-#Dijkstra
+import copy
 import heapq
 def read_puzzle(file):
     start = None
@@ -7,39 +7,56 @@ def read_puzzle(file):
     for y, line in  enumerate(open(file).readlines()):
         for x, c in enumerate(line.strip()):
             if not start and c == ".": start=(x,y)
-            d[(x,y)] = (0,None,c)
+            d[(x,y)] = c
         end = (line.index("."),y)
-    for pos in d:
-        dist, prec, c = d[pos]
-        dist = (x+1)*(y+1)
-        d[pos] = (dist, prec, c)
-    return d, x,y,start, end
+    assert(x==y)
+    return d, x,start, end
 
 DIR_SOPE = {(1,0):">", (-1,0):"<", (0,1):"v", (0,-1):"^"}
 
-def solve1(puzzle):
-    d, maxx, maxy,start, end = puzzle
-    visited = set()
-    queue = []
-    heapq.heappush(queue, ((maxx+1)*(maxy+1), start))
-    while queue:
-        dist, pos = heapq.heappop(queue)
-        if pos in visited: continue
-        if pos == end: break
-        visited.add(pos)
-        x,y = pos
-        dist_new = dist - 1
+def get_max_path_len(path, end, d, maxy):
+    if path[-1] == end: return len(path)
+
+    max_len = len(path)
+    point = path[-1]
+    x,y = point
+    path2 = copy.copy(path)
+    new_points = []
+    append = True
+    consecutive_points = []
+    while append:
+        append = False
         for delta in [(1,0),(-1,0),(0,1),(0,-1)]:
             dx,dy = delta
             point_new = (x+dx,y+dy)
             if point_new not in d: continue
-            dist_check, _, c = d[point_new]
+            if point_new in path: continue
+            if point_new in consecutive_points: continue
+            c = d[point_new]
             if c == "#": continue
-            if c != "." and c!=DIR_SOPE[delta] :continue
-            if dist_new < dist_check:
-                d[point_new] =(dist_new, pos, c)
-            heapq.heappush(queue, (dist_new, point_new))   
-    return (maxx+1)*(maxy+1) - d[end][0]
+            #if c != "." and c!=DIR_SOPE[delta] :continue
+            new_points.append(point_new)
+            if point_new == end: 
+                return max_len + len(new_points) + len(consecutive_points)
+        if len(new_points)==0:return 0
+        if len(new_points) == 1:
+            append = True
+            x, y = new_points.pop()
+            consecutive_points.append((x,y))
+    path2.extend(consecutive_points)
+    for new_p in new_points:
+        path2.append(new_p)        
+        max_len = max(max_len, get_max_path_len(path2,end,d,maxy))
+        path2.pop(-1)
+    return max_len
+
+
+def solve1(puzzle):
+    d, maxxy, start, end = puzzle
+
+    max_len = get_max_path_len([start], end,d, maxxy)
+    return max_len-1
+
 
 puzzle = read_puzzle('d23.txt')
 
